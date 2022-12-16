52f1b4779c3f98caadc299346ef71c7d132c217f
abel.uriostegui@vizio.com <Abel.Uriostegui@vizio.com>
2022-12-16 13:55:09 -0600

    Add Order solution

---
+++ tests/TP.NA.OrderServiceTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TP.NA.OrderServiceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
