66eb4d1d4fcad592f5fa71fc382c4228e64dd106
alexis.yama@globant.com <alexis.yama@globant.com>
2022-12-19 09:33:25 -0500

    Create order service

---
+++ src/TP.NA.OrderService/Data/OrderStore.cs
@@ -0,0 +1,14 @@
+﻿using TP.NA.OrderService.Models;
+
+namespace TP.NA.OrderService.Data
+{
+    public class OrderStore
+    {
+        public static List<Order> orders = new List<Order>
+        {
+            new Order(1, 1, "Order 1", DateTime.Now),
+            new Order(2, 2, "Order 2", DateTime.Now),
+            new Order(3, 3, "Order 3", DateTime.Now)
+        };
+    }
+}
---
+++ src/TP.NA.OrderService/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace TP.NA.OrderService.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public DateTime? OrderDate { get; set; }
+
+        public Order(int id, int productId, string description, DateTime? orderDate)
+        {
+            Id = id;
+            ProductId = productId;
+            Description = description;
+            OrderDate = orderDate;
+        }
+    }
+}
